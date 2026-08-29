4f16090aeebfb752eb191be40d5a710d8702f4b0
adarahman <mo_rahman@mail.com>
2026-08-29 21:10:30 +0530

    Remove unused market pipeline coordinator

--- src/application/market_pipeline/__init__.py
+++ src/application/market_pipeline/__init__.py
@@ -5,5 +5,4 @@ Split responsibilities:
 - option_chain : option chain acquisition
 - futures      : futures acquisition
 - quotes       : index quotes, VIX, ticker payloads
-- coordinator : orchestration only
 """
